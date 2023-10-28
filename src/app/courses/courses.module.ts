c23771ce6522441ed7d99e7b271ae3dbde94481a
adn90 <adonisjsouza@gmail.com>
2023-10-28 18:11:15 -0300

    Shared app material module; mat-toolbar and card

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import {MatTableModule} from '@angular/material/table';
+import { AppMaterialModule } from '../shared/app-material.module';
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
-
 
 @NgModule({
   declarations: [
@@ -14,7 +13,7 @@ import { CoursesComponent } from './courses/courses.component';
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    MatTableModule
+    AppMaterialModule,
   ]
 })
 export class CoursesModule { }
