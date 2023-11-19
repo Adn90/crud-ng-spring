ddfda2d3e4e591d5904d9456cf54eeb371de38fe
adn90 <adonisjsouza@gmail.com>
2023-11-19 15:54:38 -0300

    Mensagens de retorno ao salvar

--- src/app/courses/courses-form/courses-form.component.ts
+++ src/app/courses/courses-form/courses-form.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { HttpErrorResponse } from '@angular/common/http';
+import { Location } from '@angular/common';
 
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { CoursesService } from '../service/courses.service';
+import { ICourse } from '../model/course';
 
 @Component({
   selector: 'app-courses-form',
@@ -20,6 +22,7 @@ export class CoursesFormComponent implements OnInit {
     private formBuilder: FormBuilder,
     private courseService: CoursesService,
     private snackBar: MatSnackBar,
+    private location: Location,
     ) {
     this.form = this.formBuilder.group({
       name: [null],
@@ -34,17 +37,28 @@ export class CoursesFormComponent implements OnInit {
   onSubmit() {
     this.snackBar.dismiss();
     this.courseService.save(this.form.value).subscribe(
-      data => console.log(data), 
+      (data: ICourse) => this.onSucess(data), 
       (error: HttpErrorResponse) => {
         this.onError(error);
       } 
     );    
   }
 
-  onCancel() {}
+  onCancel() { 
+    this.snackBar.dismiss();
+    this.location.back(); 
+  }
 
   onError(error: HttpErrorResponse) {
     this.snackBar.open(error.message);
+  }
+
+  onSucess(data: ICourse) {
+    this.snackBar.open(`Curso ${data.name} foi incluído com sucesso!`, "", { duration: 3000 } );
+    setTimeout(() => {
+      this.onCancel();
+    }, 3000);    
+    
   }
 
 }
